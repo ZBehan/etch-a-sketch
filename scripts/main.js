8f87695d3f3396c853ed24dedd4742b517ff0fd7
Zach <zbehan@gmail.com>
2021-09-24 09:08:11 +1000

    Create basic createGrid function

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+const gridContainer = document.getElementById('grid');
+
+// take in user input for grid size e.g. 16 x 16
+const gridSize = 16;
+
+function createGrid(gridSize) {
+    // create 16 rows
+    // begin with rowCount at 0 and add 1 to rowCount on each pass until rowCount is equal to 16
+    for (let rowCount = 0; rowCount < gridSize; rowCount++) {
+        // create a new element called newRow
+        const newRow = document.createElement('div');
+        // add a class "row" to newRow
+        newRow.classList.add('row');
+        // create 16 cells within newRow
+        // begin with cellCount at 0 and add 1 to cellCount on each pass until cellCount is equal to 16
+        for (let cellCount = 0; cellCount < gridSize; cellCount++) {
+            // create a new element called newCell
+            const newCell = document.createElement('div');
+            // add a class "cell" to newCell
+            newCell.classList.add('cell');
+            // add newCell to newRow
+            newRow.appendChild(newCell);
+        }
+
+        // add newRow to gridContainer
+        gridContainer.appendChild(newRow);
+    }
+}
+
+createGrid(gridSize);
